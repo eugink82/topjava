95ec332087bb7475cb77c57c8e0e9cfc241f644a
Евгений <eugink82@mail.ru>
2019-06-18 16:08:57 +0300

    LP_2_5_add_dependency_injection  UserServiceImpl -добавляем сеттер для для repository  spring-app.xml -добавляем бин UserServiceImpl  SpringMain -берем бин типа UserService.class и вызываем для него метод create

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,8 +2,11 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepositoryImpl;
+import ru.javawebinar.topjava.service.UserService;
 
 import java.util.Arrays;
 
@@ -12,10 +15,10 @@ public class SpringMain {
         ConfigurableApplicationContext appCtx=new ClassPathXmlApplicationContext("spring/spring-app.xml");
         System.out.println(Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        UserRepository userRepository = (UserRepository)appCtx.getBean("inMemoryUserRepository");
-        UserRepository userRepository2= (UserRepository)appCtx.getBean(InMemoryUserRepositoryImpl.class);
+        UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
-        userRepository2.getAll();
+        UserService userService=appCtx.getBean(UserService.class);
+        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
         appCtx.close();
     }
 }
--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -12,6 +12,10 @@ import java.util.List;
 public class UserServiceImpl implements UserService{
     private UserRepository repository;
 
+    public void setRepository(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public User create(User user) {
         return repository.save(user);
