ced5ee5bf7542abce3339e080e9297df68682ea7
Евгений <eugink82@mail.ru>
2019-07-03 14:18:37 +0300

    LP_3_13_fix_servlet  index.html - изменены значения option для User-а и Admin  SecurityUtil - id получает значение на основе константы START_SEQ  MealServlet - добавлена конфигурация для работы с БД в контекст

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -26,7 +26,7 @@ public class MealServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        springContext=new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        springContext=new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/spring-db.xml");
         mealController=springContext.getBean(MealRestController.class);
     }
 
