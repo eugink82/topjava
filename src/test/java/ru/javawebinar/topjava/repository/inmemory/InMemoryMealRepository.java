61478bfc1aa17381955f0cec0330f4c4d795ac2a
Евгений <eugink82@mail.ru>
2019-07-09 23:32:47 +0300

    LP_4_2_HW3_optional   MealsUtil - MEAL_LIST удален, данные перенесены в MealTestData   InMemoryMealRepository - блок инициализации удален, данные перенесены в MealTestData   MealTestData - статические тестовые данные для работы с тестовым классом MealServiceTest   MealServiceTest - реализованы тесты из MealService для JdbcMealRepository.

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -27,11 +27,7 @@ public class InMemoryMealRepository implements MealRepository {
     private Map<Integer, InMemoryBaseRepository<Meal>> usersMealsMap = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
-    {
-        MealsUtil.MEAL_LIST.forEach(meal -> save(meal, USER_ID));
-        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
-        save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
-    }
+
 
     @Override
     public Meal save(Meal meal, int userId) {
