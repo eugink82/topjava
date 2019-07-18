6f94213d314eda451a9a89645ec7b9f98a340be8
Евгений <eugink82@mail.ru>
2019-07-18 21:45:50 +0300

    LP_5_2_fix_hibernate_issue   spring-db.xml - Из-за Hibernate bug with proxy initialization when using AccessType.FIELD в JpaMealRepository.get() делался дополнительный запрос в базу для инициализации прокси User, и мы делали хак: доступ к полю AbstractBaseEntity.id через AccessType.PROPERTY. С версии 5.2.13.Final загрузка прокси при обращении к id управляется флагом JPA_PROXY_COMPLIANCE (по умолчанию запрос не делается)   AbstractBaseEntity - Поправил equals() с учетом Lazy-проксирования

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 
 @MappedSuperclass
@@ -39,7 +41,7 @@ public abstract class AbstractBaseEntity {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) return false;
 
         AbstractBaseEntity that = (AbstractBaseEntity) o;
 
