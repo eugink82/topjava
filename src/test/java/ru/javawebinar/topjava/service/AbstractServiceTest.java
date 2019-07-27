84695c8405362274cbdefebb7d1a3493b11ec9d4
Евгений <eugink82@mail.ru>
2019-07-28 01:46:43 +0300

    LP_6_03_extract_rules   Вынес StopWatch из класса AbstractServiceTest в TimingRules

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,8 +1,10 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.AfterClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
@@ -15,6 +17,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDBProfileResolver;
+import ru.javawebinar.topjava.TimingRules;
 
 import java.util.concurrent.TimeUnit;
 
@@ -26,40 +29,13 @@ import java.util.concurrent.TimeUnit;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver= ActiveDBProfileResolver.class)
 public abstract class AbstractServiceTest {
-    private static final Logger LOG = LoggerFactory.getLogger("result");
-    private static StringBuilder timeWorkTests=new StringBuilder();
+    @ClassRule
+    public static ExternalResource summary=TimingRules.SUMMARY;
+
+    @ClassRule
+    public static Stopwatch stopWatch=TimingRules.stopwatch;
 
     @Rule
     public ExpectedException thrown=ExpectedException.none();
 
-    @Rule
-    public Stopwatch stopwatch=new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-            String result = String.format("\n%-25s %7d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            timeWorkTests.append(result);
-            LOG.info(result + " ms\n");
-            // String timeTest="Тест: "+description.getMethodName()+", время работы: "+ TimeUnit.NANOSECONDS.toMillis(nanos)+"ms";
-            // timeWorkTests.append(System.lineSeparator()).append(timeTest);
-        }
-
-        @Override
-        protected void failed(long nanos, Throwable e, Description description) {
-            String result = String.format("\n%-25s %-25s %7d", description.getMethodName(),e.getMessage(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            timeWorkTests.append(result);
-            LOG.info(result + " ms\n");
-        }
-    };
-
-
-
-    @AfterClass
-    public static void fullResults(){
-        LOG.info("\n----------------------------" +
-                "\nTest            Duration, ms" +
-                "\n----------------------------" +
-                timeWorkTests+
-                "\n----------------------------");
-        //LOG.info(timeWorkTests+"");
-    }
 }
