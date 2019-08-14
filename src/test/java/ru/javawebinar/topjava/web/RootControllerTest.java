37f5e1d31988d62a702678cac241819e1f635a6d
Евгений <eugink82@mail.ru>
2019-08-14 22:19:49 +0300

    LP_7_07_controller_test  pom.xml -добавлены зависимости harmcrest-core и harmcrest-all.С помощью harmcrest тестируются размер коллекции users(hasSize) и поля(hasItem) в классе RootControllerTest  AllActiveProfileResolver -выбор профиля по умолчанию.  RootControllerTest - добавлен тест testUsers.  AbstractControllerTest - вынесены основные характеристики RootController, описана конфигурация, определен и инциализирован MockMvc объект.

---
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class RootControllerTest extends AbstractControllerTest{
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
