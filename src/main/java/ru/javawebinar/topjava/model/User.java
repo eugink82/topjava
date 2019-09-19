6fd0db3bb2dd077d54e9e850c68e44b362774e7c
Евгений <eugink82@mail.ru>
2019-09-19 16:28:56 +0300

    LP_10_14_password_encoding_fix   User - исправил ограничение максимального значения для password до 100, т.к длина закодированного пароля больше 30

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -39,7 +39,7 @@ public class User extends AbstractNamedEntity {
 
     @Column(name = "password", nullable = false)
     @NotBlank
-    @Size(min = 5, max = 30)
+    @Size(min = 5, max = 100)
     private String password;
 
     @Column(name = "enabled", nullable = false, columnDefinition = "bool default true")
