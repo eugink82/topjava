f3d109bd807437b15fa858305361c0c7c3b26405
Евгений <eugink82@mail.ru>
2019-09-28 10:35:12 +0300

    LP_11_09_404   Обработка ошибок 404   app* - добавлена локализация "Неверного запроса"   ErrorType - добавлен тип ошибки для 404 запроса   ExceptionInfoHandler - получение корневой причины возникающего исключения и логирование вынесено в ValidationUtil   web.xml - для поля throwExceptionIfNoHandlerFound класса DispatcherServlet устанавливаем значение true.Таким образом если для запроса не был найден обработчик, то вместо 404 страницы будет генерироваться исключение NoHandlerFoundException.   GlobalControllerExceptionHandler - добавлен обработчик исключения NoHandlerFoundException

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.util;
 
+import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import ru.javawebinar.topjava.HasId;
 import ru.javawebinar.topjava.model.AbstractBaseEntity;
+import ru.javawebinar.topjava.util.exception.ErrorType;
 import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.*;
 import java.util.Set;
 import java.util.StringJoiner;
@@ -76,5 +79,15 @@ public class ValidationUtil {
 
     public static String getExceptionMessage(Throwable e){
         return e.getLocalizedMessage()!=null ? e.getLocalizedMessage() : e.getClass().getName();
+    }
+
+    public static Throwable logAndGetRootCause(HttpServletRequest req, Logger log, Exception e, boolean logException, ErrorType errorType) {
+        Throwable rootCase= ValidationUtil.getRootCause(e);
+        if(logException){
+            log.error(errorType+"at request "+req.getRequestURL().toString(),rootCase);
+        } else {
+            log.warn("{} at request {}: {}",errorType,req.getRequestURL().toString(),rootCase.toString());
+        }
+        return rootCase;
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
+++ src/main/java/ru/javawebinar/topjava/web/ExceptionInfoHandler.java
@@ -25,22 +25,21 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 import static ru.javawebinar.topjava.util.exception.ErrorType.*;
 
-@RestControllerAdvice(annotations= RestController.class)
+@RestControllerAdvice(annotations = RestController.class)
 @Order(Ordered.HIGHEST_PRECEDENCE + 5)
 public class ExceptionInfoHandler {
     private static final Logger log = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
-    public static final String EXCEPTION_DUPLICATE_EMAIL="user.duplicateEmail";
-    public static final String EXCEPTION_DUPLICATE_DATETIME="meal.duplicateDateTime";
+    public static final String EXCEPTION_DUPLICATE_EMAIL = "user.duplicateEmail";
+    public static final String EXCEPTION_DUPLICATE_DATETIME = "meal.duplicateDateTime";
 
-    private static final Map<String,String> MAP_CONSTRAINT_I18N=Map.of(
-        "users_unique_email_idx",EXCEPTION_DUPLICATE_EMAIL,
-        "meals_unique_user_datetime_idx",EXCEPTION_DUPLICATE_DATETIME
+    private static final Map<String, String> MAP_CONSTRAINT_I18N = Map.of(
+            "users_unique_email_idx", EXCEPTION_DUPLICATE_EMAIL,
+            "meals_unique_user_datetime_idx", EXCEPTION_DUPLICATE_DATETIME
     );
 
     @Autowired
@@ -48,59 +47,55 @@ public class ExceptionInfoHandler {
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(NotFoundException.class)
-    public ErrorInfo handleError(HttpServletRequest req, NotFoundException e){
-        return logAndGetErrorInfo(req,e,false,DATA_NOT_FOUND);
+    public ErrorInfo handleError(HttpServletRequest req, NotFoundException e) {
+        return logAndGetErrorInfo(req, e, false, DATA_NOT_FOUND);
     }
 
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ErrorInfo conflict(HttpServletRequest req,DataIntegrityViolationException e){
-        String rootExMessage=ValidationUtil.getRootCause(e).getMessage();
-        if(rootExMessage!=null){
-            String lowerExMessage=rootExMessage.toLowerCase();
-            Optional<Map.Entry<String,String>> entry=MAP_CONSTRAINT_I18N.entrySet().stream()
-                    .filter(elem->lowerExMessage.contains(elem.getKey()))
+    public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        String rootExMessage = ValidationUtil.getRootCause(e).getMessage();
+        if (rootExMessage != null) {
+            String lowerExMessage = rootExMessage.toLowerCase();
+            Optional<Map.Entry<String, String>> entry = MAP_CONSTRAINT_I18N.entrySet().stream()
+                    .filter(elem -> lowerExMessage.contains(elem.getKey()))
                     .findAny();
-            if(entry.isPresent()){
-                return logAndGetErrorInfo(req,e,true,VALIDATION_ERROR,messageUtil.getMessage(entry.get().getValue()));
+            if (entry.isPresent()) {
+                return logAndGetErrorInfo(req, e, true, VALIDATION_ERROR, messageUtil.getMessage(entry.get().getValue()));
             }
         }
-        return logAndGetErrorInfo(req,e,true,DATA_ERROR);
+        return logAndGetErrorInfo(req, e, true, DATA_ERROR);
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
-    public ErrorInfo validationErrors(HttpServletRequest req,Exception e){
-        BindingResult result=e instanceof BindException ? ((BindException)e).getBindingResult() :
-                ((MethodArgumentNotValidException)e).getBindingResult();
-        String[] details=result.getFieldErrors().stream().
-                map(fe->messageUtil.getMessage(fe))
+    public ErrorInfo validationErrors(HttpServletRequest req, Exception e) {
+        BindingResult result = e instanceof BindException ? ((BindException) e).getBindingResult() :
+                ((MethodArgumentNotValidException) e).getBindingResult();
+        String[] details = result.getFieldErrors().stream().
+                map(fe -> messageUtil.getMessage(fe))
                 .toArray(String[]::new);
-        return logAndGetErrorInfo(req,e,false,VALIDATION_ERROR,details);
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, details);
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler({IllegalRequestDataException.class, MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
-    public ErrorInfo illegalRequestDataError(HttpServletRequest req,Exception e){
-        return logAndGetErrorInfo(req,e,false,VALIDATION_ERROR);
+    public ErrorInfo illegalRequestDataError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR);
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ErrorInfo handleError(HttpServletRequest req,Exception e){
-        return logAndGetErrorInfo(req,e,true,APP_ERROR);
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
     }
 
 
-    private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType,String... details){
-        Throwable rootCase= ValidationUtil.getRootCause(e);
-        if(logException){
-            log.error(errorType+"at request "+req.getRequestURL().toString(),rootCase);
-        } else {
-            log.warn("{} at request {}: {}",errorType,req.getRequestURL().toString(),rootCase.toString());
-        }
-        return new ErrorInfo(req.getRequestURL(),errorType,messageUtil.getMessage(errorType.getErrorCode()),
-                details.length!=0 ? details : new String[]{ValidationUtil.getExceptionMessage(rootCase)});
+    private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType, String... details) {
+        Throwable rootCase = ValidationUtil.logAndGetRootCause(req, log, e, logException, errorType);
+        return new ErrorInfo(req.getRequestURL(), errorType, messageUtil.getMessage(errorType.getErrorCode()),
+                details.length != 0 ? details : new String[]{ValidationUtil.getExceptionMessage(rootCase)});
     }
+
 
 }
